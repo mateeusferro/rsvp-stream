71ea3bd584575ef80d2a4cc1eee7798acc756dcb
mateus <mateus.03fernandesaraujo@gmail.com>
2025-01-27 21:16:26 -0300

    init: initial commit

---
+++ src/main/java/com/ferro/mateus/rsvpstream/RsvpStreamApplication.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.rsvpstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsvpStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsvpStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ferro/mateus/rsvpstream/RsvpStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferro.mateus.rsvpstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsvpStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
